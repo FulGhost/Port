d006338aaa3d6b6e93f276e4125d429a0d49da97
Andy-fidel <Andyfideljude@gmail.com>
2026-05-24 21:05:57 +0000

    changed formatting

--- my-logbook/backend/src/prismaClient.js
+++ my-logbook/backend/src/prismaClient.js
@@ -1,10 +1,12 @@
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient} from './generated/prisma/client.ts'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client.ts";
 
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL,   ssl: {
-    rejectUnauthorized: false  // ← required for RDS
-  } });
-const prisma = new PrismaClient({ adapter })
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // ← required for RDS
+  },
+});
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
